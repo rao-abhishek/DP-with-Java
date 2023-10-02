598d8ff4b420e7a906e5f61886df73464aa41195
Abhishek Rao <59765588+rao-abhishek@users.noreply.github.com>
2023-10-02 12:40:15 +0530

    Single Input

--- Main.java
+++ Main.java
@@ -3,9 +3,13 @@ public class Main {
     public static void main(String args[])
     {
         NumberConstruct nc = new NumberConstruct();
-        System.out.println("Can Construct- " + nc.canConstruct(10, new int[]{1, 2, 3, 5, 6}));
-        System.out.println("How Many Construct- " + nc.howManyConstruct(10, new int[]{ 2,3, 5,6}));
-        System.out.println("Get All Constructs- " + nc.getAllConstructs(10, new int[]{ 2,3, 5, 6}));
-        System.out.println("Get Best Constructs- " + nc.getBestConstructs(100, new int[]{2 ,3, 5, 6}));
+        
+        int targetNum = 10;
+        int[] nums = new int[]{ 2, 3, 5, 6};
+
+        System.out.println("Can Construct- " + nc.canConstruct(targetNum, nums));
+        System.out.println("How Many Construct- " + nc.howManyConstruct(targetNum, nums));
+        System.out.println("Get All Constructs- " + nc.getAllConstructs(targetNum, nums));
+        System.out.println("Get Best Constructs- " + nc.getBestConstructs(targetNum,nums));
     }
 }
